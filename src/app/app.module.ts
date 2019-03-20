ef25ef355f13bdcabd83d635f5a09ff110a93d3d
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-03-20 08:23:58 +0200

    add test controls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
+import { NativeScriptFormsModule} from 'nativescript-angular/forms';
 
-import { AppComponent } from "./app.component";
-import { CurrentChallengeComponent } from "./challenges/current/current-challenge.component";
+import { AppComponent } from './app.component';
+import { CurrentChallengeComponent } from './challenges/current-challenge/current-challenge.component';
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
@@ -11,22 +12,13 @@ import { CurrentChallengeComponent } from "./challenges/current/current-challeng
 // import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
 
 @NgModule({
-    bootstrap: [
-        AppComponent
-    ],
-    imports: [
-        NativeScriptModule
-    ],
-    declarations: [
-        AppComponent,
-        CurrentChallengeComponent
-        ],
-    providers: [],
-    schemas: [
-        NO_ERRORS_SCHEMA
-    ]
+  bootstrap: [AppComponent],
+  imports: [NativeScriptModule, NativeScriptFormsModule],
+  declarations: [AppComponent, CurrentChallengeComponent],
+  providers: [],
+  schemas: [NO_ERRORS_SCHEMA]
 })
 /*
 Pass your application module to the bootstrapModule function located in main.ts to start your app
 */
-export class AppModule { }
+export class AppModule {}
--- src/app/challenges/current/current-challenge.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'ns-current-challange',
-    templateUrl: './current-challenge.component.html',
-    styleUrls: ['./current-challenge.component.css'],
-    moduleId: module.id
-})
-export class CurrentChallengeComponent {}
