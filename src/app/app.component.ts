6b37356077c3c4c322dad975d2d561d2a7e3e545
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-03-28 21:49:04 +0200

    test debug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  activeChalanges: string[] = [];
+  activeChalange = '';
 
   onChallengeInput(challengeDescription: string) {
-    this.activeChalanges.push(challengeDescription)
+    this.activeChalange = challengeDescription;
   }
 }
