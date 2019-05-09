e757e59a933ae8bbecff6ae6def9ee4602b10b2f
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-05-09 08:35:06 +0300

    add cel logic

--- src/app/challenges/current-challenge/current-challenge.component.ts
+++ src/app/challenges/current-challenge/current-challenge.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewContainerRef } from '@angular/core';
+import { Component, ViewContainerRef, OnInit } from '@angular/core';
 import { ModalDialogService } from 'nativescript-angular/modal-dialog';
 
 import { DayModalComponent } from '../day-modal/day-modal.component';
@@ -12,10 +12,24 @@ import { UIService } from '~/app/shared/ui.service';
     './current-challenge.component.scss'],
   moduleId: module.id
 })
-export class CurrentChallengeComponent {
+export class CurrentChallengeComponent implements OnInit {
+  weekDays = ['S','M', 'T', 'W', 'T', 'F', 'S'];
+  days: { dayInMonth: number, dayInWeek: number }[] = [];
   constructor(private modalDialog: ModalDialogService,
     private iuService: UIService,
     private vcRef: ViewContainerRef) { }
+
+  ngOnInit() {
+    const currentMonth  = new Date().getMonth();
+    const currentYear  = new Date().getFullYear();
+    const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate(); 
+
+    for (let i = 1; i < daysInMonth + 1; i++) {
+      const date = new Date(currentMonth, currentMonth, i);
+      const dayInWeek = date.getDay();
+      this.days.push({ dayInMonth: i, dayInWeek: dayInWeek });
+    }
+  }  
 
   onChangeStatus() {
     this.modalDialog.showModal(DayModalComponent, {
