6295587808e4847a85a91c2d5428b63d11238111
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-05-31 00:15:35 +0300

    add createNewChallange

--- src/app/challenges/challenge.service.ts
+++ src/app/challenges/challenge.service.ts
@@ -5,6 +5,22 @@ import { Challenge } from './challenge.model'
 
 @Injectable({ providedIn: 'root' })
 export class ChallangeService {
-    private _currentChallange = new BehaviorSubject<Challenge>(null);   
+    private _currentChallange = new BehaviorSubject<Challenge>(null);
+
+    get currentChallange() {
+        return this._currentChallange.asObservable();
+    }
+    
+    createNewChallange(title: string, description: string) {
+        const newChallenge = new Challenge(
+            title,
+            description,
+            new Date().getFullYear(),
+            new Date().getMonth()
+            );
+
+            // TODO: Save to server
+            this._currentChallange.next(newChallenge);
+    }
 }
 
