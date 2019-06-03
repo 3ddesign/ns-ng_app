a6326606cfdf632d72e66e3b564fd360995e307a
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-06-03 08:09:55 +0300

    add upadate from service

--- src/app/challenges/today/today.component.ts
+++ src/app/challenges/today/today.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { ChallangeService } from '../challenge.service';
+import { Day } from '../day.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'ns-today',
@@ -6,15 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./today.component.scss'],
   moduleId: module.id,
 })
-export class TodayComponent implements OnInit {
+export class TodayComponent implements OnInit, OnDestroy {
+  currentDay: Day;
+  private currentChallengeSub: Subscription
 
-  constructor() { }
+  constructor(private challengeService: ChallangeService) { }
 
   ngOnInit() {
+    this.currentChallengeSub = this.challengeService.currentChallange.subscribe(challenge => {
+      if (challenge) {
+        this.currentDay = challenge.currentDay;
+      }
+    });
   }
 
   onActionSelected(action: 'completed' | 'failed' | 'cancel') {
     console.log(action);
+  }
+
+  ngOnDestroy() {
+    if (this.currentChallengeSub) {
+      this.currentChallengeSub.unsubscribe();
+    }
   }
 
 }
