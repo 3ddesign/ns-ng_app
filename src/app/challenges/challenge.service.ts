6f5e36a618b4b0ed00f90c386d1fb75bc0c4d437
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-06-08 21:46:00 +0300

    day update

--- src/app/challenges/challenge.service.ts
+++ src/app/challenges/challenge.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 import { Challenge } from './challenge.model'
+import { DayStatus } from './day.model';
+
+import { take } from 'rxjs/operators'
 
 @Injectable({ providedIn: 'root' })
 export class ChallangeService {
@@ -10,17 +13,28 @@ export class ChallangeService {
     get currentChallange() {
         return this._currentChallange.asObservable();
     }
-    
+
     createNewChallange(title: string, description: string) {
         const newChallenge = new Challenge(
             title,
             description,
             new Date().getFullYear(),
             new Date().getMonth()
-            );
+        );
 
-            // TODO: Save to server
-            this._currentChallange.next(newChallenge);
+        // TODO: Save to server
+        this._currentChallange.next(newChallenge);
+    }
+
+    updateDay(dayInMonth: number, status: DayStatus) {
+        this._currentChallange.pipe(take(1)).subscribe(challenge => {
+            if (!challenge || challenge.days.length < dayInMonth) {
+                return;
+            }
+            const dayIndex = challenge.days.findIndex(day => day.dayInMonth === dayInMonth)
+            challenge.days[dayIndex].status = status;
+            this._currentChallange.next(challenge);
+        });
     }
 }
 
