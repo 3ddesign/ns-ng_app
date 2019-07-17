4d1757a914ae362d7aa5842adb6ca33dae10da15
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-07-17 07:06:12 +0300

    add handleLogin

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,33 +1,78 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { throwError, BehaviorSubject } from 'rxjs';
 import { alert } from 'tns-core-modules/ui/dialogs'
+
+import { User } from './user.model';
 
 const FIREBASE_API_KEY = 'AIzaSyDW_g5fH3LG1oQv_MLhYIOfjdvAD-UG84U';
 
+interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+  private _user = new BehaviorSubject<User>(null);
+
   constructor(private http: HttpClient) {}
 
   signUp(email: string, password: string) {
-    return this.http.post(
+    return this.http.post<AuthResponseData>(
       `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${FIREBASE_API_KEY}`,
       { email: email, password: password, returnSecureToken: true }
     ).pipe(catchError(errorResponse => {
       this.handleError(errorResponse.error.error.message);
       return throwError(errorResponse);
+    }),
+    tap(resData => {
+      if (resData && resData.idToken) {
+        this.handleLogin(
+          email,
+          resData.idToken,
+          resData.localId,
+          parseInt(resData.expiresIn)
+        );
+      }
     }));
   }
 
   login(email: string, password: string) {
-    return this.http.post(
+    return this.http.post<AuthResponseData>(
       `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${FIREBASE_API_KEY}`,
       { email: email, password: password, returnSecureToken: true }
     ).pipe(catchError(errorResponse => {
       this.handleError(errorResponse.error.error.message);
       return throwError(errorResponse);
+    }), 
+    tap(resData => {
+      if (resData && resData.idToken) {
+        this.handleLogin(
+          email,
+          resData.idToken,
+          resData.localId,
+          parseInt(resData.expiresIn)
+        );
+      }
     }));
+  }
+
+  private handleLogin(
+    email: string,
+    token: string,
+    userId: string,
+    expiresIn: number
+  ) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(email, userId, token, expirationDate);
+    this._user.next(user);
   }
 
   private handleError(errorMessage: string) {
